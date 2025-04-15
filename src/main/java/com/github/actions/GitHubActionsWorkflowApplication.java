b8bd0bb7ac22b69a4c41aab0397d44d4238c6f02
DELL <ram.singh002@yahoo.com>
2025-04-15 12:12:47 +0530

    first commit

---
+++ src/main/java/com/github/actions/GitHubActionsWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.github.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionsWorkflowApplication.class, args);
+	}
+
+}
